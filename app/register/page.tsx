d741b922e2e93b12eded649ac9f53b1efa2d6b76
Piotr Filipiak <kiddhkane@gmail.com>
2023-09-15 17:20:23 +0200

    Added error handling to register.

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,25 +1,76 @@
 "use client";
 
-import React from "react";
+import React, { useEffect } from "react";
 import { useForm, SubmitHandler, FieldValues } from "react-hook-form";
 import Link from "next/link";
+
+const url = "http://localhost:3000";
 
 const Register = () => {
   const {
     register,
     handleSubmit,
-    formState: { errors },
+    formState,
+    formState: { errors, isSubmitting, isSubmitSuccessful, isDirty },
     getValues,
+    reset,
+    setError,
+    clearErrors,
   } = useForm();
 
-  const onSubmit: SubmitHandler<FieldValues> = (data: FieldValues) => {
+  const registerUser: SubmitHandler<FieldValues> = async (
+    data: FieldValues
+  ) => {
     console.log("Using hookform: ", data);
-    console.log("Errors:", errors);
+    // console.log("Errors:", errors);
+
+    const throttle = true;
+    if (throttle) {
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+    }
+    console.log("Submitting");
+
+    const res = await fetch(`${url}/api/users/register`, {
+      method: "POST",
+      // Seems like I don't need content/type nor credentials?
+      body: JSON.stringify({ name: data.name, password: data.password }),
+    });
+
+    if (!res.ok) {
+      setError("nameTaken", { message: "User already exists." });
+    }
+    const json = await res.json();
   };
+
+  const testRegister: SubmitHandler<FieldValues> = async (
+    data: FieldValues
+  ) => {
+    console.log("Trying");
+    setError("nameTaken", { message: "User already exists." });
+  };
+
+  const onError: SubmitHandler<FieldValues> = async (data: FieldValues) => {
+    console.log("Error");
+    setError("nameTaken", { message: "User already exists." });
+  };
+
+  // Reset form if success
+  useEffect(() => {
+    // console.log("Effect");
+    if (isSubmitSuccessful) {
+      reset({
+        name: "",
+        password: "",
+        confirmPassword: "",
+      });
+    } else {
+      // console.log("Not succesful");
+    }
+  }, [formState, isSubmitSuccessful, reset]);
 
   return (
     <div className="container flex items-center">
-      <form className="form" onSubmit={handleSubmit(onSubmit)}>
+      <form className="form" onSubmit={handleSubmit(registerUser, onError)}>
         {/* Name */}
         <label className="formLabel formLabel--columns">
           Name:
@@ -27,6 +78,7 @@ const Register = () => {
             {...register("name", {
               required: "Name is required",
               minLength: { value: 3, message: "3 Characters minimum" },
+              onChange: (e) => clearErrors("nameTaken"),
             })}
             type="text"
             className="formInput"
@@ -71,7 +123,16 @@ const Register = () => {
         {errors.confirmPassword && (
           <p className="text-red-500">{`${errors.confirmPassword.message}`}</p>
         )}
-        <button className="btn btn--primary mt-8">Register</button>
+        {errors.nameTaken && (
+          <p className="text-red-500">{`${errors.nameTaken.message}`}</p>
+        )}
+        <button
+          type="submit"
+          className="btn btn--primary mt-8"
+          disabled={isSubmitting}
+        >
+          Register
+        </button>
         <p className="text-center">
           Already have an account?{" "}
           <Link href={"/login"} className="link">
