e3e8b5fd49f6092f62a143e97118b6ab80c14132
Piotr Filipiak <kiddhkane@gmail.com>
2023-09-18 19:02:52 +0200

    Adding tasks privileges.

--- app/api/tasks/route.ts
+++ app/api/tasks/route.ts
@@ -1,3 +1,4 @@
+import { getCredentials } from "@/lib/auth/credentials";
 import { usePrisma } from "@/lib/prisma";
 import { revalidatePath } from "next/cache";
 import { NextRequest, NextResponse } from "next/server";
@@ -14,6 +15,9 @@ export async function POST(request: NextRequest) {
   }
 
   const user = await usePrisma.user.findFirst({ where: { id: json.userId } });
+
+  const { id, role } = await getCredentials(request);
+
   if (!user) {
     return NextResponse.json(
       { message: "User doesn't exist." },
@@ -21,11 +25,18 @@ export async function POST(request: NextRequest) {
     );
   }
 
-  try {
-    const task = await usePrisma.task.create({ data: json });
-    revalidatePath("/");
-    return NextResponse.json(task);
-  } catch (error) {
-    return NextResponse.json({ message: error }, { status: 400 });
+  if (role === "Admin" || user.id === id) {
+    try {
+      const task = await usePrisma.task.create({ data: json });
+      revalidatePath("/");
+      return NextResponse.json(task);
+    } catch (error) {
+      return NextResponse.json({ message: error }, { status: 400 });
+    }
   }
+
+  return NextResponse.json(
+    { message: "You don't have necessary privileges." },
+    { status: 400 }
+  );
 }
